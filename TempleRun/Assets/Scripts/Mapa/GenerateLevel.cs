6b2be77e68ff66143d902ba2ed8bb897ba41ee3c
martiPJ03 <marti.puerta03@gmail.com>
2024-05-01 10:21:49 +0200

    Generate en diverses direccions

--- TempleRun/Assets/Scripts/Mapa/GenerateLevel.cs
+++ TempleRun/Assets/Scripts/Mapa/GenerateLevel.cs
@@ -8,7 +8,7 @@ public class GenerateLevel : MonoBehaviour
 
     public GameObject[] section;
 
-    public float zPos = 2.7f;
+    public float offset = 2.7f;
     public bool creatingSection = false;
     public int secNum;
 
@@ -16,6 +16,7 @@ public class GenerateLevel : MonoBehaviour
     [SerializeField] private int maximumStraightSections = 15;
 
     private Vector3 currentTileDirection = Vector3.forward;
+    private Vector3 currentTilePosition = Vector3.zero;
     private GameObject prevSection;
 
     private void Start()
@@ -31,14 +32,52 @@ public class GenerateLevel : MonoBehaviour
             creatingSection = true;
             StartCoroutine(GenerateSection());
         }
+        if (Input.GetKeyDown("a"))
+        {
+            if (currentTileDirection == Vector3.forward)
+            {
+                currentTileDirection = Vector3.left;
+            }
+            else if (currentTileDirection == Vector3.left)
+            {
+                currentTileDirection = Vector3.back;
+            }
+            else if (currentTileDirection == Vector3.back)
+            {
+                currentTileDirection = Vector3.right;
+            }
+            else
+            {
+                currentTileDirection = Vector3.forward;
+            }
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            if (currentTileDirection == Vector3.forward)
+            {
+                currentTileDirection = Vector3.right;
+            }
+            else if (currentTileDirection == Vector3.left)
+            {
+                currentTileDirection = Vector3.back;
+            }
+            else if (currentTileDirection == Vector3.back)
+            {
+                currentTileDirection = Vector3.left;
+            }
+            else
+            {
+                currentTileDirection = Vector3.forward;
+            }
+        }
     }
 
     IEnumerator GenerateSection()
     {
         secNum = UnityEngine.Random.Range(0, 3);
         Quaternion q = Quaternion.Euler(0, 90, 0);
-        Instantiate(section[secNum], new Vector3(0, 0, zPos), q);
-        zPos += 2.7f;
+        Instantiate(section[secNum], currentTilePosition + currentTileDirection*2.7f, q * Quaternion.Euler(currentTileDirection));
+        currentTilePosition += currentTileDirection * 2.7f;
         yield return new WaitForSeconds(0.8f);
         creatingSection = false;
     }
