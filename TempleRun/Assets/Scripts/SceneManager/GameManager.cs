2940aaf02a347f560f7d0b295a6dcf9e4bf08868
martiPJ03 <marti.puerta03@gmail.com>
2024-05-15 17:20:20 +0200

    menu

---
+++ TempleRun/Assets/Scripts/SceneManager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
