b2d9fca1c45dea428f913294a44b05e56d5a9e74
martiPJ03 <marti.puerta03@gmail.com>
2024-05-28 16:25:24 +0200

    Update HideElements.cs

--- TempleRun/Assets/Scripts/Camera/HideElements.cs
+++ TempleRun/Assets/Scripts/Camera/HideElements.cs
@@ -16,20 +16,27 @@ public class HideElements : MonoBehaviour
 
     void HideObjectsBetweenCameraAndTarget()
     {
-        Vector3 direction = new Vector3( target.position.x, target.position.y+1, target.position.z )- transform.position;
+        // Calculate the direction and distance between the camera and the target
+        Vector3 direction = new Vector3(target.position.x, target.position.y + 1, target.position.z) - transform.position;
         float distance = direction.magnitude;
+
+        // Perform a raycast to detect all objects in the path
         RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, distance, obstacleLayer);
 
         foreach (RaycastHit hit in hits)
         {
             GameObject hitObject = hit.collider.gameObject;
+
+            // Check if the object has the specified tag
             if (hitObject.tag == "EliminarCamera")
             {
+                // Change the layer of the hit object
                 hitObject.layer = LayerMask.NameToLayer(hiddenLayerName);
-                GameObject[] sons = hitObject.GetComponentsInChildren<GameObject>();
-                foreach (GameObject son in sons)
+
+                // Iterate through all child transforms and change their layers as well
+                foreach (Transform child in hitObject.transform)
                 {
-                    son.layer = LayerMask.NameToLayer(hiddenLayerName);
+                    child.gameObject.layer = LayerMask.NameToLayer(hiddenLayerName);
                 }
             }
         }
